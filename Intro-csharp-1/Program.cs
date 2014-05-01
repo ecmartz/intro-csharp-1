08044a86bf39b87aed07fcbe2923be3be2c2074a
Esteban Martinez <ecmartz.nm@gmail.com>
2014-05-01 13:58:34 -0600

    Part 8: arrays

--- Intro-csharp-1/Program.cs
+++ Intro-csharp-1/Program.cs
@@ -4,51 +4,20 @@
     {
         static void Main()
         {
-            /* PART 7
-             * Datatype Conversion
+            /* PART 8
+             * Arrays
              * 
-             * Implicit/Explicit Conversions
-             * Parse(), TryParse()
-             * Use Parse() if you are sure the value will be valid
-             * Use TryParse() otherwise
-             * 
-             * Implicit conversions done by the compiler:
-             * 1. Implicit conversion is done by the compiler when no loss of information if conversion is done
-             * 2. No possibility of throwing exceptions during conversion
-             * 
-             * Error handling
-             * Type cast: Will convert the types willy-nilly
-             * Convert Class: Will throw an exception if conversion fails
+             * Pros: Arrays are strongly typed
+             * Cons: Arrays cannot grow in size once initialized. Have to rely on integral indices to retrieve items from array.
              */
 
-            // Automagically!
-            //int i = 100;
-            //float f = i;
+            int[] evennumbers = new int[3];
 
-            float f = 123.45F;
+            evennumbers[0] = 0;
+            evennumbers[1] = 2;
+            evennumbers[2] = 4;
+            evennumbers[3] = 6;
 
-            int i = (int)f;                         // (int) type cast 
-            int j = Convert.ToInt32(f);             // .ToInt32 Convert class
-
-            Console.WriteLine("Type cast: " + i);
-            Console.WriteLine("Convert class: {0}", j);
-
-
-            /* Using Parse() */
-            string strnumber = "100";
-            int k = int.Parse(strnumber);
-
-            Console.WriteLine("int.Parse = {0}", k);
-
-            /* Using TryParse() */
-            string strnum = "12345";
-            int result = 0;
-
-            bool isConversionSuccessful = int.TryParse(strnum, out result);
-
-            if (isConversionSuccessful)
-                Console.WriteLine(result);
-            else
-                Console.WriteLine("Please enter a valid number");
+            Console.WriteLine(evennumbers[1]);
         }
     }
