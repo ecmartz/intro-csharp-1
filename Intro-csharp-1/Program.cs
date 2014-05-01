ef296d94173e4721441c87fc6f7a09e80ca3f273
Esteban Martinez <ecmartz.nm@gmail.com>
2014-05-01 11:54:14 -0600

    Part 5: operators

--- Intro-csharp-1/Program.cs
+++ Intro-csharp-1/Program.cs
@@ -4,16 +4,52 @@
     {
         static void Main()
         {
-            // Escape sequence
-            string sassyname = "\"Esteban\"";
-            Console.WriteLine(sassyname);
+            // Assignment operator
+            int i = 10;
+            bool b = true;
 
-            // Newline charcters
-            string newline = "one\ntwo\nthree";
-            Console.WriteLine(newline);
+            // Quotient and modulo operator
+            int numerator = 10;
+            int denominator = 2;
 
-            // Verbatim Literal
-            string verbatim = @"C:\REPOS\Intro-csharp-1\";
-            Console.WriteLine(verbatim);
+            int result = numerator / denominator;
+            int result2 = numerator % denominator;
+
+            Console.WriteLine("Quotient = {0}", result);
+            Console.WriteLine("Remainder = {0}", result2);
+
+         
+            int number = 10;
+            int anothernumber = 20;
+
+            // Comparison operator
+            if(number == 10)
+            {
+                Console.WriteLine("number is 10");
+            }
+
+            // AND comparison operator
+            if(number == 10 && anothernumber == 20)
+            {
+                Console.WriteLine("number is 10 AND anothernumber is 20");
+            }
+
+            // Program control flow
+            bool isnumber10;
+
+            if(number == 10)
+            {
+                isnumber10 = true;
+            }
+            else
+            {
+                isnumber10 = false;
+            }
+
+            Console.WriteLine("number == 10 is {0}", isnumber10);
+
+            // Ternary operator
+            bool isnum10 = number == 10 ? true : false;
+            Console.WriteLine("Ternary: number == 10 is {0}", isnum10);
         }
     }
