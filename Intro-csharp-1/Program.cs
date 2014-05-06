0c22437474a01cc920a5f9aeede93a39065bd2e9
Esteban Martinez <ecmartz.nm@gmail.com>
2014-05-06 10:13:50 -0600

    Part 9: comments

--- Intro-csharp-1/Program.cs
+++ Intro-csharp-1/Program.cs
@@ -4,20 +4,25 @@
     {
         static void Main()
         {
-            /* PART 8
-             * Arrays
-             * 
-             * Pros: Arrays are strongly typed
-             * Cons: Arrays cannot grow in size once initialized. Have to rely on integral indices to retrieve items from array.
+            /*
+             * PART 9 Comments
+             * Single line and multi-line comments
+             * XML documentation comments
              */
+            
+            // Single line comment
 
-            int[] evennumbers = new int[3];
-
-            evennumbers[0] = 0;
-            evennumbers[1] = 2;
-            evennumbers[2] = 4;
-            evennumbers[3] = 6;
-
-            Console.WriteLine(evennumbers[1]);
+            /* Multi
+             * Line
+             * Comment
+             */
         }
+    }
+
+/// <summary>
+/// This is a sample class XML documentation summary
+/// </summary>
+    public class SampleClass
+    {
+
     }
