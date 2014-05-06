2a31c85cf957738fababd46b4dc80de6b682d9ac
Esteban Martinez <ecmartz.nm@gmail.com>
2014-05-06 10:27:28 -0600

    Part 10: if statements

--- Intro-csharp-1/Program.cs
+++ Intro-csharp-1/Program.cs
@@ -5,24 +5,42 @@
         static void Main()
         {
             /*
-             * PART 9 Comments
-             * Single line and multi-line comments
-             * XML documentation comments
+             * PART 10 If Statements
+             * 
+             * if(<condition>) { <execute> }
+             * else if(<condition>) { <execute> }
+             * else { <execute> }
+             * 
+             * OR:
+             * |    Compiler checks both conditions for true
+             * ||   Compiler only checks the first condition for true
+             * 
+             * AND:
+             * &    Compiler checks both conditions for false
+             * &&   Compiler only checks first condition for false
+             * 
+             * Doubles are sometimes referred to as "short circuit" operators
+             * 
              */
-            
-            // Single line comment
 
-            /* Multi
-             * Line
-             * Comment
-             */
+            Console.WriteLine("Please enter a number");
+            int usernum = int.Parse(Console.ReadLine());
+
+            if (usernum == 1)
+            {
+                Console.WriteLine("Your number is 1.");
+            }
+            else if (usernum == 2)
+            {
+                Console.WriteLine("Your number is 2.");
+            }
+            else if (usernum == 3)
+            {
+                Console.WriteLine("Your number is 3.");
+            }
+            else
+            {
+                Console.WriteLine("Your number is not 1,2, or 3.");
+            }
         }
-    }
-
-/// <summary>
-/// This is a sample class XML documentation summary
-/// </summary>
-    public class SampleClass
-    {
-
     }
