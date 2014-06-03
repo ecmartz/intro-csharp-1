4f8f288d202ef1a7b6247364a59d818e0aec6e26
Esteban Martinez <ecmartz.nm@gmail.com>
2014-06-03 15:52:42 -0600

    Part 17: Method parameters

--- Intro-csharp-1/Program.cs
+++ Intro-csharp-1/Program.cs
@@ -2,47 +2,72 @@
 
     class Program
     {
-        public static void Main()       // Static method
+        public static void Main()
         {
             /*
-             * PART 17: Methods
+             * PART 17: Method parameters
              * http://youtu.be/c4hOXIG8yPo
              */
 
-            // Methods
-            // [attributes]
-            // access-modifiers return-type method-name ( parameters ) { method body }
+            /* Parameters (4 types)
+             * Value parameters         : Passes a copy
+             * Reference parameters     : with the 'ref' keyword, passes the reference
+             * Out parameters           : Return more than one value using 'out' keyword
+             * Parameter arrrays        : Lets you pass a variable amount of params using 'params' keyword
+             */
 
-            // To invoke an instance method, you have to invoke an instance of that class
-            // To invoke a static method, you have to invoke the class
+            /* Arguments    : values that are passed in the invoking call
+             * Parameters  :  values that are passed in the method declaration
+             */
 
-            Program p = new Program();
-            p.evenNumbers(50);                  // invoke instance method
-            Program.evenNums();                 // invoke static method
+            int i = 0;
+            int total, product;
+            int[] nums = new int[3];
 
-            int sum = p.addIt(10, 20);
-            Console.WriteLine("Sum = {0}", sum);
-            
+            nums[0] = 101;
+            nums[1] = 102;
+            nums[2] = 103;
+
+            simpleMethod(i);                        // Pass by value
+            Console.WriteLine(i);
+
+            simpMethod(ref i);                      // Pass by references
+            Console.WriteLine(i);
+
+            calcUlate(10, 20, out total, out product); // Multiple outputs
+
+            paramsMethod(nums);                     // Parameter array
+
+            Console.WriteLine("Sum = {0} && Product = {1}", total, product);
         }
 
-        public int addIt(int fn, int sn)
+        /* Value parameter usage */
+        public static void simpleMethod(int j)      // Pass by value
         {
-            return fn + sn;
+            j = 101;
         }
 
-        public void evenNumbers(int target)       // Instance method
+        /* Reference parameter usage */
+        public static void simpMethod(ref int j)    // Pass by reference
         {
-            int start = 0;
+            j = 101;
+        }
 
-            while (start <= target)
+        /* Out parameter usage */
+        public static void calcUlate(int fn, int sn, out int sum, out int product) // Returning multiple values
+        {
+            sum = fn + sn;
+            product = fn * sn;
+
+        }
+
+        /* Parameter array usage */
+        public static void paramsMethod(params int[] nums) // Essentially makes parameter arrays optional
+        {
+            Console.WriteLine("There are {0} elements.",nums.Length);
+            foreach (int i in nums)
             {
-                Console.WriteLine(start);
-                start = start + 2;
+                Console.WriteLine(i);
             }
-        }
-
-        public static void evenNums()              // Static method
-        {
-
         }
     }
