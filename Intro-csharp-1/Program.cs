cb1f37b2d5bdb0eacc17d350f8754a72cd714afc
Esteban Martinez <ecmartz.nm@gmail.com>
2014-06-03 18:34:43 -0600

    Part 21: Inheritance

--- Intro-csharp-1/Program.cs
+++ Intro-csharp-1/Program.cs
@@ -5,52 +5,79 @@
         public static void Main()
         {
             /*
-             * PART 20: Static and instance class members
-             * http://youtu.be/cFQLmHCguGs
-             * Static class members
-             * Instance class members
-             *  their differences
+             * PART 21: Inheritance
+             * http://youtu.be/OLTk0xAVY00
              * 
-             * 'this' refers to the current instance of the object
-             * static constructors cannot have access modifiers
-             * Class members w/o access modifiers are implied 'private'
+             * Inheritance syntax,
+             * derived class : base class
              * 
-             * Static class constructors are used to initialize static fields
-             * Static constructors are initialized before istance constructors
-             *  and even before you refer to any field in that class!
+             * FOUR PILLARS OF OOP
+             * 1. Inheritance
+             * 2. Encapsulation
+             * 3. Abstraction
+             * 4. Polymorphism
+             * 
+             * Each class can only have ONE base class
+             * C# supports multiple interface inheritance
+             * Base classes are automatically instantiated before inherited classes
+             * Using base(), you can determine which constructor the child class will run
+             * 
              */
 
-            Circle C1 = new Circle(5);
-            float area = C1.calculateArea();
-            Console.WriteLine("Area = {0}", area);
+            FullTimeEmployee FTE = new FullTimeEmployee();
+            FTE.firstName = "Joe";
+            FTE.lastName = "Blowe";
+            FTE.yearlySalary = 50000;
+            FTE.printFullName();
 
-            Circle C2 = new Circle(6);
-            float area2 = C2.calculateArea();
-            Console.WriteLine("Area = {0}", area2);
+            PartTimeEmployee PTE = new PartTimeEmployee();
+            PTE.firstName = "Joel";
+            PTE.lastName = "Blow";
+            PTE.hourlyRate = 10;
+            PTE.printFullName();
+
+            ChildClass CC = new ChildClass();
         }
     }
 
-class Circle
+public class Employee
 {
-    //static float _PI = 3.141F;  // Single shared copy of _PI for all instances
-    // float _PI = 3.141F;        // Copy of _PI for each instance of Circle
-    static float _PI;
-    int _radius;
+    public string firstName;
+    public string lastName;
+    public string email;
 
-    static Circle()
+    public void printFullName()
     {
-        Console.WriteLine("Static constructor called");
-        Circle._PI = 3.141F;
+        Console.WriteLine(firstName + " " + lastName);
     }
+}
 
-    public Circle(int radius)
+public class FullTimeEmployee : Employee
+{
+    public float yearlySalary;
+}
+
+public class PartTimeEmployee : Employee
+{
+    public float hourlyRate;
+}
+
+public class ParentClass
+{
+    public ParentClass()
     {
-        Console.WriteLine("Instance constructor called");
-        this._radius = radius;
+        Console.WriteLine("Parent Constructor called.");
     }
-
-    public float calculateArea()
+    public ParentClass(string message)
     {
-        return Circle._PI * this._radius * this._radius;
+        Console.WriteLine(message);
+    }
+}
+
+public class ChildClass : ParentClass
+{
+    public ChildClass() : base("Derived class controlling Parent class")
+    {
+        Console.WriteLine("Child Constructor called.");
     }
 }
